33b983b7d13be3df1207c78a7630ad53e054555e
Gerd Busker <gerd@busker.org>
2016-10-18 06:09:02 +0100

    fixed issue with return char

--- process_buttons.sh
+++ process_buttons.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-url="http://lighting.dev.smartgaiacloud.com/diagCode/demo2_api.php?kitcode=944A0CE6F141&eventCode="
+url='http://lighting.dev.smartgaiacloud.com/diagCode/demo2_api.php?kitcode=944A0CE6F141&eventCode='
 
 while true ; do
   read command value
   case "$command" in
     event)
       echo "$command: $value"
-      echo "$url$value"
+      cleanurl=$(echo "$url$value" | tr -d '\r')
+      curl $cleanurl
       ;;
   esac
 done
